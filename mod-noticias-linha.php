6c7b901204c0f878dbd527c6c6cd21c4985ad710
adaildoneto <adaildo.neto@gmail.com>
2019-02-06 14:58:11 -0500

    segunda linha de materias

--- mod-noticias-linha.php
+++ mod-noticias-linha.php
@@ -1,4 +1,35 @@
 <div class="row">
+  <div class="col s12 m12 l6">
+    <?php
+
+      $myargs = array (
+        'pagination'             => false,
+        'category_name'					=> 'geral, policia, mundo, meio ambiente',
+        'posts_per_page'         => 1,
+        'offset'                  => 2,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+
+
+          get_template_part( 'slider', '' );
+
+
+                wp_reset_postdata();
+
+        }
+
+      ?>
+  </div>
+
 <div class="col s12 m12 l6">
 
 
@@ -20,7 +51,7 @@
           $myquery->the_post();
 
             echo ('<div class="col s12 ">');
-          get_template_part( 'cardnews', 'hgrande' );
+          get_template_part( 'cardnews', 'destaque' );
               echo ('</div>')  ;
 
                 wp_reset_postdata();
@@ -30,36 +61,7 @@
       ?>
 
 </div>
-    <div class="col s12 m12 l6">
-      <?php
 
-        $myargs = array (
-          'pagination'             => false,
-          'category_name'					=> 'economia',
-          'posts_per_page'         => 1,
-          'offset'                  => 2,
-          'ignore_sticky_posts'    => true,
-
-        );
-        // The Query
-        $myquery = new WP_Query( $myargs );
-
-        // The Loop
-        while ( $myquery->have_posts() ) {
-
-            $myquery->the_post();
-
-
-
-            get_template_part( 'slider', '' );
-
-
-                  wp_reset_postdata();
-
-          }
-
-        ?>
-    </div>
 </div>
 
 <div class="row">
@@ -83,7 +85,7 @@
 
         echo ('<div class="col s12 m6 l3">');
 
-        get_template_part( 'cardnews', 'destaque' );
+        get_template_part( 'cardnews', '' );
 
         echo ('</div>')  ;
               wp_reset_postdata();
