85e54da394e183d29b1565a59e8eeb79640912ed
adaildoneto <adaildo.neto@gmail.com>
2019-02-07 00:38:25 -0500

    corrigindo erro responsivo

--- cardnews-hgrande.php
+++ cardnews-hgrande.php
@@ -4,7 +4,7 @@
 
               <div class="card-image">
                       <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'big-quadrado' );?>
-                <img class="responsive-img img-hgrande" src="<?php echo $image[0]; ?>" >
+                <img class="responsive-img" src="<?php echo $image[0]; ?>" >
               </div>
               <div class="card-stacked">
                 <div class="">
