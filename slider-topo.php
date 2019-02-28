dc7b50e520a9278ba1d5400524beca4bb46d8dd3
adaildoneto <adaildo.neto@gmail.com>
2019-02-27 23:47:45 -0500

    agora só falta o post

--- slider-topo.php
+++ slider-topo.php
@@ -1,7 +1,7 @@
   <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'slider' );?>
 <div class="card slider-post-topo" style="background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0) 52%,rgba(0,0,0,0.65) 84%,rgba(0,0,0,0.65) 100%), url('<?php echo $image[0]; ?>'); background-size: cover; background-position: center;">
 
-          <div class="center center-align" style="position: absolute; bottom: 50px;">
+          <div class="center center-align" style="position: absolute; bottom: 50px; padding-right: 10%; padding-left: 10%;">
             <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
             <h3 id="responsive_headline" class="white-text" >
               <b> <?php the_title();?> </b>
