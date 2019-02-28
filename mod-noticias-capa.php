52eaf557766b8e517ec021d8b42387085b34a029
adaildoneto <adaildo.neto@gmail.com>
2019-02-27 22:11:41 -0500

    buhg

--- mod-noticias-capa.php
+++ mod-noticias-capa.php
@@ -32,6 +32,34 @@
                         ?>
           </div>
 
+          <div class="col s12">
+
+          <?php
+
+            $myargs = array (
+              'pagination'             => false,
+              'category_name'					=> '',
+              'posts_per_page'         => 2,
+              'ignore_sticky_posts'    => true,
+
+            );
+            // The Query
+            $myquery = new WP_Query( $myargs );
+
+            // The Loop
+            while ( $myquery->have_posts() ) {
+
+                $myquery->the_post();
+
+                      get_template_part( 'cardnews', 'hgrande' );
+
+                  wp_reset_postdata();
+
+              }
+
+            ?>
+        </div>
+
           <div class="col S12 m12 l6">
 
               <?php
@@ -52,9 +80,9 @@
                     $myquery->the_post();
 
                     if ( $myquery->current_post == 0 )  {
-                    get_template_part( 'cardnews', 'hgrande' );
+                    get_template_part( 'cardnews', 'horizontal' );
                   } else {
-                    get_template_part( 'cardnews', 'hgrande' );
+                    get_template_part( 'cardnews', 'horizontal' );
 
 
                   }
