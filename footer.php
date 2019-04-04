0d72627e23ff9dac68d2f1775dec8598652957ea
adaildoneto <adaildo.neto@gmail.com>
2019-04-04 09:55:48 -0500

    sa

--- footer.php
+++ footer.php
@@ -14,7 +14,28 @@
 
 	<footer id="footer" role="contentinfo">
 		<div class="container">
-			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
+			<div class="row">
+		            <?php if ( dynamic_sidebar('publicidade') ) : else : endif; ?>
+		          </div>
+
+
+
+		        <div class="row">
+
+		            <div class="col l4 m12 s12">
+		                <?php dynamic_sidebar( 'rodape1' ); ?>
+		                <!-- Chamando formulario do Odin -->
+
+		            </div>
+		            <div class="col l5 m12 s12">
+		                <?php dynamic_sidebar( 'rodape2' ); ?>
+		            </div>
+		            <div class="col l3 m12 s12">
+		                <?php dynamic_sidebar( 'rodape3' ); ?>
+		            </div>
+		        </div>
+
+			<p class="center-align">&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
 		</div><!-- .container -->
 	</footer><!-- #footer -->
 
