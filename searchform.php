7f0b9d51113b55037996ae729194cb40bff1821a
adaildoneto <adaildo.neto@gmail.com>
2019-04-04 11:31:08 -0500

    aASD

--- searchform.php
+++ searchform.php
@@ -7,14 +7,14 @@
  */
 ?>
 
-<div class="row">
+<nav class="white">
 	<div class="nav-wrapper">
-				<form>
-					<div class="input-field">
-					 <input id="search" type="search" placeholder="Pesquisa" value="<?php echo get_search_query(); ?>" name="s"/>
-					 <label class="label-icon active" for="search"><i class="material-icons right">search</i></label>
-					 <i class="material-icons">close</i>
-					 </div>
-				 </form>
+		<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<div class="input-field white-text">
+			 <input id="search" type="search" placeholder="Pesquisa" value="<?php echo get_search_query(); ?>" name="s"/>
+				<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+				<i class="material-icons">close</i>
+			</div>
+		</form>
 	</div>
-</div>
+</nav>
