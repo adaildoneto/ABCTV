444a23b0e694438da7cacf1663d0d138c10cbab9
adaildoneto <adaildo.neto@gmail.com>
2019-04-07 11:34:38 -0500

    sa

--- mod-colunas.php
+++ mod-colunas.php
@@ -2,61 +2,56 @@
   <div id="caluna" class="colunas">
 
 
-    <div class="colunista col s12 m6 l3 center">
-    <img class="circle responsive-img z-depth-1 center-align img-coluna coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
+    <div class="colunista">
+    <img align="center" class="circle  z-depth-1 img-coluna center-align coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
 
     <div class=""><a href=""><h4 class=" news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
     </div>
 
     </div>
 
-    <div class="colunista col s12 m6 l3 center">
-    <img class="circle responsive-img z-depth-1 center-align img-coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
+    <div class="colunista">
+    <div><img align="center" class="circle  z-depth-1 img-coluna center-align" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif"></div>
 
     <div class=""><a href=""><h4 class=" news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
     </div>
 
     </div>
 
-    <div class="colunista col s12 m6 l3 center">
-    <img class="circle responsive-img z-depth-1 center-align img-coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
+    <div class="colunista">
+    <div><img align="center" class="circle  z-depth-1 img-coluna center-align" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif"></div>
 
     <div class=""><a href=""><h4 class=" news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
     </div>
 
     </div>
 
-    <div class="colunista col s12 m6 l3 center">
-    <img class="circle responsive-img z-depth-1 center-align img-coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
+    <div class="colunista">
+    <div><img align="center" class="circle  z-depth-1 img-coluna center-align" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif"></div>
 
     <div class=""><a href=""><h4 class=" news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
     </div>
 
     </div>
 
-    <div class="colunista col s12 m6 l3 center">
-    <img class="circle responsive-img z-depth-1 center-align img-coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
+    <div class="colunista">
+
+    <div><img align="center" class="circle  z-depth-1 img-coluna center-align" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif"></div>
+
+    <div class=""><a href=""><h4 class="news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
+    </div>
+
+    </div>
+
+    <div class="colunista">
+    <div><img align="center" class="circle  z-depth-1 img-coluna center-align" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif"></div>
 
     <div class=""><a href=""><h4 class=" news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
     </div>
 
     </div>
 
-    <div class="colunista col s12 m6 l3 center">
-    <img class="circle responsive-img z-depth-1 center-align img-coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
 
-    <div class=""><a href=""><h4 class=" news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
-    </div>
-
-    </div>
-
-    <div class="colunista col s12 m6 l3 center">
-    <img class="circle responsive-img z-depth-1 center-align img-coluna" src="https://3dejulhonoticias.com.br/wp-content/uploads/2018/12/47591999_1013389978845318_4001141784347410432_n-e1544499406711.gif">
-
-    <div class=""><a href=""><h4 class=" news2 blue-grey-text text-darken-4 center-align-align-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Coluna do Crica</h4></a>
-    </div>
-
-    </div>
 
 
 </div>
