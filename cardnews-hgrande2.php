6d93bd936911378fac7dd28a4d1d9a8a8d2ec165
adaildoneto <adaildo.neto@gmail.com>
2019-04-08 23:13:17 -0500

    ok

--- cardnews-hgrande2.php
+++ cardnews-hgrande2.php
@@ -1,14 +1,14 @@
-
+<div class="">
+    <?php $categories = get_the_category();
+        if ( ! empty( $categories ) ) {
+            echo '<span class="chip principalchip blue-grey lighten-4 black-text">'. esc_html( $categories[0]->name ) . '</span>';
+            }?>
+</div>
   <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
         <div class="card transparent horizontal z-depth-0">
 
               <div class="card-stacked">
-                <div class="">
-                    <?php $categories = get_the_category();
-                        if ( ! empty( $categories ) ) {
-                            echo '<span class="chip principalchip blue-grey lighten-4 black-text">'. esc_html( $categories[0]->name ) . '</span>';
-                            }?>
-                </div>
+
                 <div class="news2 blue-grey-text text-darken-4">
                   <?php the_title();?>
 
