669ce0ee5e98a4a58b3a72d75a976b43e27cf492
adaildoneto <adaildo.neto@gmail.com>
2019-04-11 13:49:44 -0500

    update

--- cardnews-hgrande.php
+++ cardnews-hgrande.php
@@ -1,8 +1,12 @@
 <div class="" style="margin-bottom: 20px;">
+  <div class="">
+      <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'big-quadrado' );?>
+      <img class="responsive-img show-on-small hide-on-med-and-up" src="<?php echo $image[0]; ?>" >
+  </div>
   <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
         <div class="card transparent horizontal z-depth-0">
 
-              <div class="card-image">
+              <div class="card-image hide-on-small-only show-on-medium-and-up">
                       <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'big-quadrado' );?>
                 <img class="responsive-img" src="<?php echo $image[0]; ?>" >
               </div>
@@ -17,7 +21,7 @@
                   <?php the_title();?>
 
                   </div>
-                
+
                   <div class="news-text blue-grey-text text-darken-4 hide-on-med-and-down">
                                   <?php echo excerpt('30'); ?>
                                 </div>
--- cardnews-hgrande2.php
+++ cardnews-hgrande2.php
@@ -4,23 +4,34 @@
             echo '<span class="chip principalchip blue-grey lighten-4 black-text">'. esc_html( $categories[0]->name ) . '</span>';
             }?>
 </div>
-  <a href="<?php the_Permalink()?>" title="<?php the_title();?>" >
+
+
+
         <div class="card transparent horizontal z-depth-0">
 
-              <div class="card-stacked">
+              <div class="textot">
 
-                <div class="news2 blue-grey-text text-darken-4">
+                <div class="news2">
+                      <a class="blue-grey-text text-darken-4" href="<?php the_Permalink()?>" title="<?php the_title();?>" >
                   <?php the_title();?>
-
+                                </a>
                   </div>
 
               </div>
 
-              <div class="card-image">
-                            <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'toponews' );?>
-                <img class="responsive-img" src="<?php echo $image[0]; ?>" >
+              <div class="">
 
+                            <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'toponews' );?>
+
+
+                <img class="responsive-img hide-on-small-only show-on-medium-and-up" src="<?php echo $image[0]; ?>" >
+                      </a>
                   </div>
             </div>
-</a>
+
+            <div class="">
+                <?php $image =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'topo' );?>
+                <img class="responsive-img show-on-small hide-on-med-and-up" src="<?php echo $image[0]; ?>" >
+            </div>
+
     <hr class="style-one">
