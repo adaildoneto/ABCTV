874260c11100d53b6a8f72dfa4f559a152432d81
adaildoneto <adaildo.neto@gmail.com>
2019-04-12 01:49:26 -0500

    destaques ok

--- mod-noticias-linha.php
+++ mod-noticias-linha.php
@@ -7,8 +7,8 @@
 
     $myargs = array (
       'pagination'             => false,
-      'cat'			  		         => array ('12'),
-      'posts_per_page'         => 4,
+      'cat'			  		         => array ('19'),
+      'posts_per_page'         => 1,
       'ignore_sticky_posts'    => true,
 
     );
@@ -30,7 +30,89 @@
       }
 
     ?>
+    <?php
 
+      $myargs = array (
+        'pagination'             => false,
+        'cat'			  		         => array ('20'),
+        'posts_per_page'         => 1,
+        'ignore_sticky_posts'    => true,
+
+      );
+      // The Query
+      $myquery = new WP_Query( $myargs );
+
+      // The Loop
+      while ( $myquery->have_posts() ) {
+
+          $myquery->the_post();
+
+          echo ('<div class="col s12 m6 l3">');
+
+          get_template_part( 'cardnews', '' );
+
+          echo ('</div>')  ;
+                wp_reset_postdata();
+
+        }
+
+      ?>
+
+      <?php
+
+        $myargs = array (
+          'pagination'             => false,
+          'cat'			  		         => array ('21'),
+          'posts_per_page'         => 1,
+          'ignore_sticky_posts'    => true,
+
+        );
+        // The Query
+        $myquery = new WP_Query( $myargs );
+
+        // The Loop
+        while ( $myquery->have_posts() ) {
+
+            $myquery->the_post();
+
+            echo ('<div class="col s12 m6 l3">');
+
+            get_template_part( 'cardnews', '' );
+
+            echo ('</div>')  ;
+                  wp_reset_postdata();
+
+          }
+
+        ?>
+
+        <?php
+
+          $myargs = array (
+            'pagination'             => false,
+            'cat'			  		         => array ('22'),
+            'posts_per_page'         => 1,
+            'ignore_sticky_posts'    => true,
+
+          );
+          // The Query
+          $myquery = new WP_Query( $myargs );
+
+          // The Loop
+          while ( $myquery->have_posts() ) {
+
+              $myquery->the_post();
+
+              echo ('<div class="col s12 m6 l3">');
+
+              get_template_part( 'cardnews', '' );
+
+              echo ('</div>')  ;
+                    wp_reset_postdata();
+
+            }
+
+          ?>
 
 </div>
 
