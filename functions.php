85d55e93d4226445b39ed356ed516ac073db64bf
adaildoneto <adaildo.neto@gmail.com>
2019-04-12 13:09:41 -0500

    aA

--- functions.php
+++ functions.php
@@ -511,3 +511,23 @@ function wp_pagination($pages = '', $range = 9)
     if ( !empty($wp_query->query_vars['s']) ) $pagination['add_args'] = array( 's' => get_query_var( 's' ) );
     echo '<div class="wp_pagination">'.paginate_links( $pagination ).'</div>';
 }
+
+add_filter( 'get_the_archive_title', function ($title) {
+
+    if ( is_category() ) {
+
+            $title = single_cat_title( '', false );
+
+        } elseif ( is_tag() ) {
+
+            $title = single_tag_title( '', false );
+
+        } elseif ( is_author() ) {
+
+            $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+
+        }
+
+    return $title;
+
+});
