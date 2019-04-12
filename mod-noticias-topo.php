6ce983ea61958bdcf0a9d0aa0648f4bf0a282d85
adaildoneto <adaildo.neto@gmail.com>
2019-04-12 01:43:57 -0500

    dasdas

--- mod-noticias-topo.php
+++ mod-noticias-topo.php
@@ -38,7 +38,7 @@
       $myargs = array (
         'pagination'             => false,
         'cat'					          => array ('9'),
-        'posts_per_page'         => 3,
+        'posts_per_page'         => 1,
         'ignore_sticky_posts'    => true,
 
       );
@@ -59,6 +59,60 @@
         }
 
       ?>
+
+      <?php
+
+        $myargs = array (
+          'pagination'             => false,
+          'cat'					          => array ('12'),
+          'posts_per_page'         => 1,
+          'ignore_sticky_posts'    => true,
+
+        );
+        // The Query
+        $myquery = new WP_Query( $myargs );
+
+        // The Loop
+        while ( $myquery->have_posts() ) {
+
+            $myquery->the_post();
+
+              echo ('<div class="col s12 ">');
+            get_template_part( 'cardnews', 'hgrande2' );
+                echo ('</div>')  ;
+
+                  wp_reset_postdata();
+
+          }
+
+        ?>
+
+        <?php
+
+          $myargs = array (
+            'pagination'             => false,
+            'cat'					          => array ('13'),
+            'posts_per_page'         => 1,
+            'ignore_sticky_posts'    => true,
+
+          );
+          // The Query
+          $myquery = new WP_Query( $myargs );
+
+          // The Loop
+          while ( $myquery->have_posts() ) {
+
+              $myquery->the_post();
+
+                echo ('<div class="col s12 ">');
+              get_template_part( 'cardnews', 'hgrande2' );
+                  echo ('</div>')  ;
+
+                    wp_reset_postdata();
+
+            }
+
+          ?>
 
 </div>
 
