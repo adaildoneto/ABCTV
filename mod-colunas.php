8d1876ddb5c53d25b7a3cde945613e46cf08e08f
adaildoneto <adaildo.neto@gmail.com>
2019-06-16 13:22:10 -0500

    sa

--- mod-colunas.php
+++ mod-colunas.php
@@ -4,12 +4,12 @@
 <div class="row">
   <div id="caluna" class="colunas">
 
-    <div class="colunista">
+  <!--  <div class="colunista">
     <div class="center-align"><img align="center" class="circle  z-depth-1 img-coluna center-align" src="https://diariodoacre.com.br/wp-content/uploads/2019/04/FB_IMG_1554598089479-1-150x150.jpg"></div>
 
     <div class="row"><a href="http://diariodoacre.com.br/category/archibaldo-antunes/"><h4 class=" news2 blue-grey-text text-darken-4 center-align" style="font-size: 1.2rem; font-weight: 800; padding: 5px;">Calibre .38</h4></a>
       <h6 class="blue-grey-text text-darken-4 center-align" style="font-size: 0.8rem; margin-top: -15px;">Archibaldo Antunes</h6></a>
-      <?php
+      <
 
         $myargs = array (
           'pagination'             => false,
@@ -35,9 +35,9 @@
 
           }
 
-        ?>
+        >
 
-    </div>
+    </div> -->
 
     </div>
     <div class="colunista">
