efc1f4ca3c17d36980e12d85d085546a453621e0
adaildoneto <adaildo.neto@gmail.com>
2019-12-10 18:51:04 -0500

    sa

--- footer.php
+++ footer.php
@@ -42,13 +42,13 @@
 								<?php dynamic_sidebar( 'rodape3' ); ?>
 						</div>
 
-			<p class="center-align">&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
-	</div>	<!-- .container -->
+		</div>	<!-- .container -->
 
 	</footer><!-- #footer -->
 </div>
 </div>
 
+<p class="center-align">&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
 
 
 	<?php wp_footer(); ?>
