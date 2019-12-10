bea5887250f9b439f64c53e91eb866bf0db598a5
adaildoneto <adaildo.neto@gmail.com>
2019-12-10 15:13:09 -0500

    google adsense

--- header.php
+++ header.php
@@ -22,13 +22,7 @@
 	<?php if ( ! get_option( 'site_icon' ) ) : ?>
 		<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" rel="shortcut icon" />
 	<?php endif; ?>
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-1803151208547694",
-    enable_page_level_ads: true
-  });
-</script>
+<script data-ad-client="ca-pub-1803151208547694" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<?php wp_head(); ?>
 </head>
 
